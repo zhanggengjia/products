5bae6a8dd6df3bc5a950fb40730ad496d87b912c
zhanggengjia <r2328643@gmail.com>
2020-11-01 03:48:57 +0800

    add csv system

--- products.py
+++ products.py
@@ -9,3 +9,8 @@ print (products)
 
 for product in products:
 	print(product[0],"的價格是",product[1])
+
+with open('product.csv', 'w', encoding='utf-8') as f:
+	f.write('商品,價格\n')
+	for product in products:
+		f.write(product[0]+ ','+ product[1]+ '\n')
